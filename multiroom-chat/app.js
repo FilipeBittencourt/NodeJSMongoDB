ce7af2ad6ad22eecb62fd043a029397df5b6bc52
filipe <filipe@sisnet.com.br>
2017-06-05 13:57:02 -0300

    multiroom  100%

--- multiroom-chat/app.js
+++ multiroom-chat/app.js
@@ -1,5 +1,51 @@
 const app = require('./config/server');
 
-app.listen(3001, () => {
+const server = app.listen(3001, () => {
     console.log('Servidor rodando na porta... 3001');
+});
+
+const io = require('socket.io').listen(server);
+app.set('io', io);
+
+io.on('connection', (socket) => {
+    console.log("usuário conectou");
+
+    socket.on("disconnect", () => {
+        console.log("usuário desconectou");
+    });
+
+    socket.on("msgParaServidor", (data) => {
+        //Dialogo
+        socket.emit(
+            'msgParaCliente',
+            {
+                apelido: data.apelido,
+                mensagem: data.mensagem
+            }
+        );
+        socket.broadcast.emit(
+            'msgParaCliente',
+            {
+                apelido: data.apelido,
+                mensagem: data.mensagem
+            }
+        );
+        //Participantes
+        if (parseInt(data.apelidoAtualizadoNosClientes) === 0) {
+            socket.emit(
+                'participantesParaCliente',
+                {
+                    apelido: data.apelido
+                }
+            );
+            socket.broadcast.emit(
+                'participantesParaCliente',
+                {
+                    apelido: data.apelido
+                }
+            );
+        }
+
+    });
+
 });
--- multiroom-chat/app/controllers/chat.js
+++ multiroom-chat/app/controllers/chat.js
@@ -2,8 +2,14 @@ module.exports.iniciaChat = function (application, req, res) {
     req.assert('apelido', 'Apelido ou nome obrigatório').notEmpty();
     req.assert('apelido', 'Apelido ou nome deve conter entre 3 e 15 caracteres').len(3, 15);
     if (req.validationErrors()) {
-        res.render("index", {validaco: req.validationErrors()});
+        res.render("index", { validacao: req.validationErrors() });
         return;
     }
-    res.render("chat");
+    application.get('io').emit(
+        'msgParaCliente',
+        {
+            apelido: req.body.apelido,
+            mensagem: 'acabou de entrar no chat'
+        });
+    res.render("chat", { dadosForm: req.body.apelido });
 };
