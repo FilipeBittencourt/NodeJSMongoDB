f07b4593c32882270ef3a6f6da3b9c98ae1173eb
filipe <filipe@sisnet.com.br>
2017-06-06 13:23:02 -0300

    Seção 15 aula 83 - inicio

--- mmorpg-game-of-thrones/app/routes/jogo.js
+++ mmorpg-game-of-thrones/app/routes/jogo.js
@@ -1,5 +1,5 @@
 module.exports = function (application) {
-    application.get('/', function (req, res) {
+    application.get('/jogo', function (req, res) {
         res.render('jogo');
     });
 }
